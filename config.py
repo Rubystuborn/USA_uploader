780d0bf0c6bbd198312085b80d3a15f2582bd4a6
Stubborn <131737791+stubbornlove@users.noreply.github.com>
2023-09-11 11:14:56 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6356959346:AAGvm-QcAzzlRC7613apZNgpj_7NfvQcTzU")
+    API_ID = int(os.environ["API_ID", 20543083]
+    API_HASH = os.environ["API_HASH", "8c0491f39646893e947098b6133c44cb"]
+    AUTH_USERS = "6146689795 """
+
